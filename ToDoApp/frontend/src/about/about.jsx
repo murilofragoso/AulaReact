77571f182475b713fb621f4a80d891e5b52f25cf
Murilo Fragoso <mu_fragoso@hotmail.com>
2021-03-06 11:01:42 -0300

    Aula 06/03/2021

--- ToDoApp/frontend/src/about/about.jsx
+++ ToDoApp/frontend/src/about/about.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre nós" small="6º ADS"/>
+        <h2>Nossa história</h2>
+        <p>Somos a turma mais TOP</p>
+        <h2>Missão e Visão</h2>
+        <p>Vem com o 6º ADS</p>
     </div>
 )
